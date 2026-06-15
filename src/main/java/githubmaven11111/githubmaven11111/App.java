b15ec8e68d91e88f7599d97527e29856dca2786e
Suvarna <suvarnagangbo@gmail.com>
2026-06-15 17:19:36 +0530

    azure pipeline 1

---
+++ src/main/java/githubmaven11111/githubmaven11111/App.java
@@ -0,0 +1,13 @@
+package githubmaven11111.githubmaven11111;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
